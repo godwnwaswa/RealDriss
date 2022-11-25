b6ea4bb1c6358c7de0e0b156cf4377541db4b3c1
Godwin Waswa <godwnwaswa@gmail.com>
2022-11-26 01:40:02 +0300

    websocket related composer update

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,6 +38,8 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => true,
+                'host' => '127.0.0.1',
+                'port' => 6001,     
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
@@ -62,6 +64,17 @@ return [
             'driver' => 'null',
         ],
 
+    ],
+
+    'apps' => [
+        [
+            'id' => env('PUSHER_APP_ID'),
+            'name' => env('APP_NAME'),
+            'key' => env('PUSHER_APP_KEY'),
+            'secret' => env('PUSHER_APP_SECRET'),
+            'enable_client_messages' => false,
+            'enable_statistics' => true,
+        ],
     ],
 
 ];
